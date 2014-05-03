8b75068881cd9f62f36fe8ca2b0c8fa08630889b
Jonathan Tsai <jontsai@jonathantsai.com>
2014-05-03 19:44:52 +0000

    add answer for 049

--- code_challenges/project_euler/python/049.py
+++ code_challenges/project_euler/python/049.py
@@ -3,16 +3,17 @@
 Prime permutations
 
 The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways: (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.
-There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property, but there is one other 4-digit increasing sequence.
+There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this 
+property, but there is one other 4-digit increasing sequence.
 What 12-digit number do you form by concatenating the three terms in this sequence?
 
 Solution by jontsai <hello@jontsai.com>
 """
 from utils import *
 
-EXPECTED_ANSWER = 0
+EXPECTED_ANSWER = 296962999629
 
-existing = 1487
+existing_answer = 148748178147
 
 lower = 1000
 upper = 9999
@@ -30,8 +31,9 @@ for prime in primes:
     # find all 4-digit prime permutations in the set
     permutations_of_prime = filter(lambda x: x >= lower, prime_permutations(prime))
     subseries = arithmetic_series_subset(permutations_of_prime)
-    if len(subseries) >= 3 and subseries[0] != existing:
+    if len(subseries) >= 3:
         answer = ''.join([str(n) for n in subseries])
-        break
+        if answer != existing_answer:
+            break
 
 print 'Expected: %s, Answer: %s' % (EXPECTED_ANSWER, answer)
