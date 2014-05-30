db2f95fd85836e61c11f779f6c1a5566a77f4fad
Jonathan Tsai <jontsai@jonathantsai.com>
2014-05-30 07:25:35 +0000

    further improved efficiency of 27 by reducing search space

--- code_challenges/project_euler/python/027.py
+++ code_challenges/project_euler/python/027.py
@@ -33,10 +33,41 @@ max_consecutive_primes = 0
 best_a = 0
 best_b = 0
 
-for a in xrange(-limit + 1, limit):
-    for b in xrange(1, limit):
-        if not is_prime(b):
-            continue
+primes_below_limit = generate_primes(limit)[:]
+
+# initial bounds:
+# -1000 < a < 1000
+# -1000 < b < 1000
+#
+# Initial search space is O(1999 * 1999)
+# Or approximately O(2000 * 2000)
+#
+# However, b must be positive and odd, since:
+# When n = 0:
+#   n^2 + an + b
+#   (0)^2 + a * 0 + b = b
+#
+# Now, search space has been reduced to O(2000 * 500)
+#
+# Let b be odd
+# Suppose a is even
+# When n is even:
+#   n^2 + an + b
+#   (even * even) + (even * even) + odd
+#   even + even + odd = odd
+#
+# When n is odd:
+#   (odd * odd) + (even * odd) + odd
+#   odd + even + odd = even
+#
+# So when a is even:
+# the most consecutive primes that can be generated is 2 (n = 0, n = 1)
+#
+# So a must be odd
+# Now, search space has been reduced to O(1000 * 500)
+
+for a in xrange(-limit + 1, limit, 2):
+    for b in primes_below_limit:
         unbroken_prime_chain = True
         n = 0
         consecutive_primes = 0
@@ -57,4 +88,3 @@ print str(best_a), str(best_b), max_consecutive_primes
 answer = best_a * best_b
 
 print 'Expected: %s, Answer: %s' % (EXPECTED_ANSWER, answer)
-
