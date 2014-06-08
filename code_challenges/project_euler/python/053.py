7cccb5c6020670b6c9dce6d69a5eb4babba4147f
Jonathan Tsai <jontsai@jonathantsai.com>
2014-06-08 01:13:06 +0000

    added solution for 53

--- code_challenges/project_euler/python/053.py
+++ code_challenges/project_euler/python/053.py
@@ -1,4 +1,4 @@
-http://projecteuler.net/problem=053
+"""http://projecteuler.net/problem=053
 
 Combinatoric selections
 
@@ -14,3 +14,24 @@ nCr = n!/(r!(n-r)!), where r <= n, n! = nx(n-1)x...x3x2x1, and 0! = 1.
 It is not until n = 23, that a value exceeds one-million: 23C10 = 1144066.
 
 How many, not necessarily distinct, values of nCr, for 1 <= n <= 100, are greater than one-million?
+
+Solution by jontsai <hello@jontsai.com>
+"""
+from utils import *
+
+EXPECTED_ANSWER = 0
+
+lower = 1
+upper = 100
+
+count = 0
+
+for n in xrange(lower, upper + 1):
+    for r in xrange(1, n + 1):
+        ncr = factorial(n) / (factorial(r) * factorial(n - r))
+        if ncr > 1000000:
+            count += 1
+
+answer = count
+
+print 'Expected: %s, Answer: %s' % (EXPECTED_ANSWER, answer)
